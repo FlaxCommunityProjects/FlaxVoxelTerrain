7a1afbb1c6e0dcc7434b553bd9647514b9c7bdff
Lukáš Jech <klukule@moowdesign.eu>
2019-08-29 08:24:21 +0200

    Set of noise generators and modifiers, and Double based Vector3 class

---
+++ Source/FlaxVoxel/World/TerraGen/Noise/ClampNoise.cs
@@ -0,0 +1,36 @@
+﻿namespace FlaxVoxel.TerraGen.Noise
+{
+    public class ClampNoise : NoiseGen
+    {
+        public NoiseGen Noise { get; set; }
+        public double MinValue { get; set; }
+        public double MaxValue { get; set; }
+
+        public ClampNoise(NoiseGen noise)
+        {
+            Noise = noise;
+            MinValue = 0;
+            MaxValue = 1;
+        }
+
+        public override double Value2D(double x, double y)
+        {
+            var NoiseValue = Noise.Value2D(x, y);
+            if (NoiseValue < MinValue)
+                NoiseValue = MinValue;
+            if (NoiseValue > MaxValue)
+                NoiseValue = MaxValue;
+            return NoiseValue;
+        }
+
+        public override double Value3D(double x, double y, double z)
+        {
+            var NoiseValue = Noise.Value3D(x, y, z);
+            if (NoiseValue < MinValue)
+                NoiseValue = MinValue;
+            if (NoiseValue > MaxValue)
+                NoiseValue = MaxValue;
+            return NoiseValue;
+        }
+    }
+}
---
+++ Source/FlaxVoxel/World/TerraGen/Noise/InvertNoise.cs
@@ -0,0 +1,21 @@
+﻿namespace FlaxVoxel.TerraGen.Noise
+{
+    public class InvertNoise : NoiseGen
+    {
+        public NoiseGen Noise { get; set; }
+        public InvertNoise(NoiseGen Noise)
+        {
+            this.Noise = Noise;
+        }
+
+        public override double Value2D(double X, double Y)
+        {
+            return -Noise.Value2D(X, Y);
+        }
+
+        public override double Value3D(double X, double Y, double Z)
+        {
+            return -Noise.Value3D(X, Y, Z);
+        }
+    }
+}
